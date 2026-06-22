4a76781afd1412224d43f0f45b6825c9de100dfb
siva gowri <gowri12chinnapureddy@gamil.com>
2026-06-22 16:32:44 +0530

    first version

---
+++ EmployeeManagementSysytem/src/main/java/com/ems/dao/ConnectionManager.java
@@ -0,0 +1,30 @@
+package com.ems.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionManager {
+ public static Connection getConnection() {
+	 String driver = "com.mysql.cj.jdbc.Driver";
+	 String url = "jdbc:mysql://localhost:3306/advems";
+	 	 String username = "root";
+		 String password = "root";
+		 
+		 Connection connection = null;
+		 try {
+			Class.forName(driver);
+			connection = DriverManager.getConnection(url,username,password);
+			connection.setAutoCommit(false);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	 return connection;
+ }
+}
+
---
+++ EmployeeManagementSysytem/src/main/java/com/ems/service/SearchEmployee.java
@@ -0,0 +1,35 @@
+package com.ems.service;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.coyote.Request;
+
+import com.ems.dao.EmployeeDAO;
+import com.ems.model.Employee;
+
+
+@WebServlet("/findByEmail")
+public class SearchEmployee extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String email = request.getParameter("email");
+		EmployeeDAO dao = new EmployeeDAO();
+		Employee employee = dao.searchEmployee(email);
+	if(employee.getId() !=0) {
+		RequestDispatcher rd = request.getRequestDispatcher("viewemp.jsp");
+		request.setAttribute("employee", employee);
+		rd.forward(request, response);
+	}else {
+		RequestDispatcher rd = request.getRequestDispatcher("viewemp.jsp");
+		request.setAttribute("employee", employee);
+		rd.forward(request, response);
+	}
+	}
+
+} 
